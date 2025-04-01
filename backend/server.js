fa0c427407d1488e2c4b3610140908f8f2fddd71
Eduardo Karpfenstein <edukarp10@gmail.com>
2025-04-01 13:38:35 -0300

    fix: add vercel PORT to connect

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Conexão com o MongoDB
-require("dotenv").config();
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
